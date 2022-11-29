446e06f016a63f9cf28e63a63b04b0662c5d3bd8
walled99 <74975873+walled99@users.noreply.github.com>
2022-11-29 22:41:50 +0200

    Add files via upload

---
+++ display_details.js
@@ -0,0 +1,43 @@
+function display() {
+  const p = document.getElementById("p");
+  const btn = document.getElementById("btn");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn.style.visibility = "hidden";
+}
+
+function display1() {
+  const p = document.getElementById("p1");
+  const btn1 = document.getElementById("btn1");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn1.style.visibility = "hidden";
+}
+function display2() {
+  const p = document.getElementById("p2");
+  const btn1 = document.getElementById("btn2");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn1.style.visibility = "hidden";
+}
+function display3() {
+  const p = document.getElementById("p3");
+  const btn1 = document.getElementById("btn3");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn1.style.visibility = "hidden";
+}
+function display4() {
+  const p = document.getElementById("p4");
+  const btn1 = document.getElementById("btn4");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn1.style.visibility = "hidden";
+}
+function display5() {
+  const p = document.getElementById("p5");
+  const btn1 = document.getElementById("btn5");
+  p.style.visibility = "visible";
+  p.style.borderBottomStyle = "groove";
+  btn1.style.visibility = "hidden";
+}
